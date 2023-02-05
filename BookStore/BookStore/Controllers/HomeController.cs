981b84bc3f31efdffb422cb3cdd23a646b90cd90
Thel2022 <r.r.pendrick@gmail.com>
2023-02-05 18:07:46 +0200

    Compilation error test commit

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace BookStore.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
