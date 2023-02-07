08b7265ea5aed3c5e64f11e3a5fded4cac16ea96
Thel2022 <r.r.pendrick@gmail.com>
2023-02-07 18:14:14 +0200

    Checkpoint commit (VERY BIG freezes/Don't work?)

---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string ISBN { get; set; }
+        public string Genre { get; set; }
+        public string Annotation { get; set; }
+        public string PublishingHouse { get; set; }
+        public string CoverType { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Format { get; set; }
+        public int Weight { get; set; }
+        public decimal Price { get; set; }
+        public string Cover { get; set; }
+
+    }
+}
